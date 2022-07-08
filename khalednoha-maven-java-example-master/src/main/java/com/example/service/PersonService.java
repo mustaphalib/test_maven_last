3ae4de978144c8ba15d9fba693678d8512f4ff94
Mustapha ALIBOURKI <64364329+mustaphalib@users.noreply.github.com>
2022-07-09 00:09:38 +0200

    addd

--- khalednoha-maven-java-example-master/src/main/java/com/example/service/PersonService.java
+++ khalednoha-maven-java-example-master/src/main/java/com/example/service/PersonService.java
@@ -25,6 +25,12 @@ Person p = new Person();
 		p.setLastName("Amrosy");
 		persons.add(p);
 		
+		Person p = new Person();
+		p.setId("1");
+		p.setAge(21);
+		p.setFirstName("Khaled");
+		p.setLastName("Amrosy");
+		persons.add(p);
 		
 		
 		
