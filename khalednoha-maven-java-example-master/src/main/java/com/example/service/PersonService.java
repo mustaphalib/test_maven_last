d41ce2ae6b9c142ce71c668c1232cb64051295e4
Mustapha ALIBOURKI <64364329+mustaphalib@users.noreply.github.com>
2022-07-08 19:54:38 +0200

    delete

--- khalednoha-maven-java-example-master/src/main/java/com/example/service/PersonService.java
+++ khalednoha-maven-java-example-master/src/main/java/com/example/service/PersonService.java
@@ -26,12 +26,7 @@ public class PersonService {
 		p.setLastName("Shalaby");
 		persons.add(p);
 		
-		p = new Person();
-		p.setId("3");
-		p.setAge(25);
-		p.setFirstName("Basem");
-		p.setLastName("Salama");
-		persons.add(p);
+		
 		
 		
 		
